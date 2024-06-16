bc289f3dd45d198b2e84c307d21005a5f50f589e
sudheer0071 <sudheer1614@gmail.com>
2024-06-17 00:03:27 +0530

    build issues resolved

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const { router } = require("./routes");   
 const cors = require('cors')
@@ -5,10 +6,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+const port = process.env.PORT || 4000
 app.use('/api/v1',router) 
 
 app.get('/',(req,res)=>{
   res.send("backend is working fine")
 })
-app.listen(3000) 
+app.listen(port,()=>{
+  console.log(`server is listening on port ${port}`);
+}) 
